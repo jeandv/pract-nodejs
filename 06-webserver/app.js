97773c0a934885285bf78b714965972d8bd692d4
Jean Rondón <jeanrdev@gmail.com>
2023-01-19 22:54:31 -0400

    seccion-6

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -1,9 +1,25 @@
+require('dotenv').config();
 const express = require('express');
+const hbs = require('hbs');
 const app = express();
+
+const port = process.env.PORT;
+
+// Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+// Servir contenido estatico
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
 
-  res.send('Hello jean, welcome to Home!');
+  // res.send('Hello jean, welcome to Home!');
+  res.render('home', {
+    nombre: 'Jean Rondón',
+    usuario: 'jeandv',
+    titulo: 'Curso de Node'
+  });
 
 });
 
@@ -13,10 +29,33 @@ app.get('/hola-mundo', (req, res) => {
 
 });
 
-app.get('*', (req, res) => {
+app.get('/generic', (req, res) => {
 
-  res.send('404 | Page not found');
+  // res.sendFile(__dirname + '/public/generic.html');
+  res.render('generic', {
+    nombre: 'Jean Rondón',
+    usuario: 'jeandv',
+    titulo: 'Curso de Node'
+  });
 
 });
 
-app.listen(8080);
+app.get('/elements', (req, res) => {
+
+  // res.sendFile(__dirname + '/public/elements.html');
+  res.render('elements', {
+    nombre: 'Jean Rondón',
+    usuario: 'jeandv',
+    titulo: 'Curso de Node'
+  });
+
+});
+
+app.get('*', (req, res) => {
+
+  // res.send('404 | Page not found');
+  res.sendFile(__dirname + '/public/404.html');
+
+});
+
+app.listen(port);
