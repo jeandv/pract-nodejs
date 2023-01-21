ddc51969f19408414284df038746cb12deca2f04
Jean Rondón <jeanrdev@gmail.com>
2023-01-21 17:39:07 -0400

    seccion-8

---
+++ 08-restserver-cafe/db/config.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
+
+const dbConnection = async () => {
+
+  try {
+
+    await mongoose.connect(process.env.MONGODB_CNN, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+
+    console.log('Base de datos online')
+
+  } catch (error) {
+    console.log(error);
+    throw new Error('Error al iniciar la Base de Datos');
+  }
+
+}
+
+module.exports = {
+  dbConnection
+};
---
+++ 08-restserver-cafe/models/server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const cors = require('cors');
+const { dbConnection } = require('../db/config');
+
+class Server {
+
+  constructor() {
+
+    this.app = express();
+    this.port = process.env.PORT;
+    this.usuariosPath = '/api/usuarios';
+
+    // Conectar a base de datos
+    this.conectarDB();
+
+    // Middlewares
+    this.middlewares();
+
+    // Rutas de mi aplicación
+    this.routes();
+
+  }
+
+  async conectarDB() {
+    await dbConnection();
+  }
+
+  middlewares() {
+
+    // CORS
+    this.app.use(cors());
+
+    // Lextura y parseo del body
+    this.app.use(express.json());
+
+    // Directorio publico
+    this.app.use(express.static('public'));
+
+  }
+
+  routes() {
+
+    this.app.use(this.usuariosPath, require('../routes/user'))
+
+  }
+
+  listen() {
+
+    this.app.listen(this.port, () => console.log('Server running on port:', this.port));
+
+  }
+
+}
+
+module.exports = Server;
