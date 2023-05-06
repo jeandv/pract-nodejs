691695c43f34cb07eed4df06ceff6cb72a9fcdec
jeandv <jeanrdev@gmail.com>
2023-05-05 22:47:48 -0400

    validate rol with DB

--- 08-restserver-cafe/routes/user.js
+++ 08-restserver-cafe/routes/user.js
@@ -3,6 +3,8 @@ const { check } = require('express-validator');
 
 const { validarCampos } = require('../middlewares/validar-campos');
 
+const Role = require('../models/role');
+
 const {
   usuariosGet,
   usuariosPut,
@@ -21,7 +23,16 @@ router.post('/', [
   check('nombre', 'El nombre es obligatorio').not().isEmpty(),
   check('password', 'El password es obligatorio y/o debe de ser mas de 6 letras').isLength({ min: 6 }).not().isEmpty(),
   check('correo', 'Correo no válido').isEmail(),
-  check('rol', 'No es un rol permitido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+  // check('rol', 'No es un rol permitido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+  check('rol').custom(async (rol = '') => {
+
+    const existeRol = await Role.findOne({ rol });
+
+    if (!existeRol) {
+      throw new Error(`El rol ${rol} no esta registrado en la DB`)
+    }
+
+  }),
   validarCampos
 ], usuariosPost);
 
