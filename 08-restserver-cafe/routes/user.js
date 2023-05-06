3de6883e6d248253c8cd3b545356c74f84d3e158
jeandv <jeanrdev@gmail.com>
2023-05-05 22:06:31 -0400

    validate all inputs and create the middleware validar-campo

--- 08-restserver-cafe/routes/user.js
+++ 08-restserver-cafe/routes/user.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
+const { validarCampos } = require('../middlewares/validar-campos');
+
 const {
   usuariosGet,
   usuariosPut,
@@ -16,7 +18,11 @@ router.get('/', usuariosGet);
 router.put('/:id', usuariosPut);
 
 router.post('/', [
-  check('correo', 'Correo no válido').isEmail()
+  check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+  check('password', 'El password es obligatorio y/o debe de ser mas de 6 letras').isLength({ min: 6 }).not().isEmpty(),
+  check('correo', 'Correo no válido').isEmail(),
+  check('rol', 'No es un rol permitido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+  validarCampos
 ], usuariosPost);
 
 router.delete('/', usuariosDelete);
