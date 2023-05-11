b695190df22796596593d0c09d1627fdb7008375
jeandv <jeanrdev@gmail.com>
2023-05-11 01:44:29 -0400

    Delete: Borrando usuario de la DB de dos formas Fisicamente y actualizando su estado a false para siempre tener los datos guardado en la DB pero al hacer GET no aparezca gracias a la validacion de siempre mostrar los que tienen el estado en true

--- 08-restserver-cafe/routes/user.js
+++ 08-restserver-cafe/routes/user.js
@@ -33,7 +33,12 @@ router.post('/', [
   validarCampos
 ], usuariosPost);
 
-router.delete('/', usuariosDelete);
+router.delete('/:id', [
+  check('id', 'No es un ID valido').isMongoId(),
+  check('id').custom(existeUsuarioPorId),
+  validarCampos
+],
+  usuariosDelete);
 
 router.patch('/', usuariosPatch);
 
