cd8a5b76ea3e66d7399a0f70a294ce5c60cc6432
jeandv <jeanrdev@gmail.com>
2023-05-23 16:19:14 -0400

    proteger rutas mediante uso de Token JWT (creando el middleware validarJWT)

--- 08-restserver-cafe/controllers/users.js
+++ 08-restserver-cafe/controllers/users.js
@@ -66,13 +66,16 @@ const usuariosDelete = async (req, res = response) => {
 
   const { id } = req.params;
 
+  // const { uid } = req.uid;
+
   // fisicamente lo borramos
   // const usuario = await Usuario.findByIdAndDelete(id);
 
   const usuario = await Usuario.findByIdAndUpdate(id, { estado: false });
 
   res.json({
-    usuario
+    usuario,
+    // uid
   });
 
 };
--- 08-restserver-cafe/routes/user.js
+++ 08-restserver-cafe/routes/user.js
@@ -2,6 +2,8 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 
 const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+
 const { esRoleValido, emailExiste, existeUsuarioPorId } = require('../helpers/db-validators');
 
 const {
@@ -34,6 +36,7 @@ router.post('/', [
 ], usuariosPost);
 
 router.delete('/:id', [
+  validarJWT,
   check('id', 'No es un ID valido').isMongoId(),
   check('id').custom(existeUsuarioPorId),
   validarCampos
