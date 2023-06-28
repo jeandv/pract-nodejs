52433aae892900467810b9de579a460420ac001e
jeandv <jeanrdev@gmail.com>
2023-06-28 02:41:53 -0400

    add crearCategoria (error)

---
+++ 08-restserver-cafe/controllers/categorias.js
@@ -0,0 +1,46 @@
+const { response } = require('express');
+
+const { Categoria } = require('../models/categoria');
+
+
+const crearCategoria = async (req, res = response) => {
+
+  try {
+
+    const nombre = req.body.nombre.toUpperCase();
+
+    const categoriaDB = await Categoria.findOne({ nombre });
+
+    if (categoriaDB) {
+      return res.status(400).json({
+        msg: `La categoria ${categoriaDB.nombre}, ya existe`
+      });
+    }
+
+    // generar la data a guardar
+    const data = {
+      nombre,
+      usuario: req.usuario._id
+    }
+
+    const categoria = new Categoria(data);
+    // guardar en DB
+    await categoria.save();
+
+    res.status(201).json(categoria);
+
+  } catch (error) {
+
+    console.log(error);
+
+    return res.json({
+      msg: "Hable con el Administrador"
+    });
+
+  }
+
+}
+
+module.exports = {
+  crearCategoria
+}
--- 08-restserver-cafe/routes/categorias.js
+++ 08-restserver-cafe/routes/categorias.js
@@ -2,6 +2,9 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 
 const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+const { crearCategoria } = require('../controllers/categorias');
 
 const router = Router();
 
@@ -16,9 +19,11 @@ router.get('/:id', (req, res) => {
 });
 
 // crear categoria - privado - cualquier persona con un token valido
-router.post('/', (req, res) => {
-  res.json('post');
-});
+router.post('/', [
+  validarJWT,
+  check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+  validarCampos
+], crearCategoria);
 
 // actualizar un registro por el ID - privado, cualquier persona con un token valido
 router.put('/:id', (req, res) => {
